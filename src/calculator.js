18be2ddc1883f315d6ed8b75325f38eff3cf411b
Mayur Gawade <mayurgawade1339@gmail.com>
2025-01-20 22:31:26 +0530

    Implement substract() method to calculator

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,9 @@ class Calculator {
     add(a, b) {
       return a + b;
     }
+    subtract(a, b) {
+        return a - b;
+      }
   }
   
 export default Calculator;
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,12 +1,23 @@
 import { expect } from 'chai';
 import Calculator from '../src/calculator.js';
 
-describe('Calculator', function() {
+describe('Calculator', function() {  
+    let calculator;
+
+    beforeEach(function() {
+      calculator = new Calculator();
+    });
   describe('#add()', function() {
     it('should return the sum of two numbers', function() {
-      const calculator = new Calculator();
       const result = calculator.add(2, 3);
       expect(result).to.equal(5);
+    });
+  });
+
+  describe('#subtract()', function() {
+    it('should return the difference of two numbers', function() {
+      const result = calculator.subtract(5, 3);
+      expect(result).to.equal(2);
     });
   });
 });
